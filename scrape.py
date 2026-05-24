0f17772def36d8d9ac4af2058e3fdc1d1e34f8f1
WisamaliUcll <muhammad1wisam@gmail.com>
2026-05-24 19:47:54 +0200

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -69,7 +69,7 @@ for game in games:
 
     rows.append({
         "title": title,
-        "price_tag": price_eur,
+        "price_tag": price_tag,
         "discount": discount,
         "release_date": release_date,
         "platforms": ", ".join(platforms),
